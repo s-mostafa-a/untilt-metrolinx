d2b71150b350cdc1c8dadc6857f0bf1880ae8a7e
Mostafa <s.mostafa.a96@gmail.com>
2021-10-22 12:16:12 -0400

    minor refactoring

--- un_tilt.py
+++ un_tilt.py
@@ -2,36 +2,34 @@ import os
 
 import numpy as np
 
-# uncomment all the comment to visualize the point cloud
-# import open3d as o3d
+import open3d as o3d
 
 ROTATION_MATRIX = np.array([[0.99648296, 0.03442719, -0.07639682],
                             [-0.01334566, 0.9652704, 0.26091177],
                             [0.08272604, -0.25897457, 0.96233496]])
+# you may want to change these variables
+SOURCE_DIRECTORY = './data/source'
+OUTPUT_DIRECTORY = './data/output'
 
 
-def rotate_csv(file_name, source_directory, output_directory):
-    # pcd = o3d.geometry.PointCloud()
-    path_to_point_cloud = os.path.join(source_directory, file_name)
+def rotate_csv(file_name, visualize=False):
+    path_to_point_cloud = os.path.join(SOURCE_DIRECTORY, file_name)
     points_with_i = np.genfromtxt(path_to_point_cloud, dtype=None, delimiter=',', skip_header=0)
     points = points_with_i[:, :3]
     rotated_points_transpose = ROTATION_MATRIX @ points.T
     rotated_points_transpose = rotated_points_transpose * np.array([[-1], [-1], [1]])
     rotated_points = rotated_points_transpose.T
     points_with_i[:, :3] = rotated_points
-    # pcd.points = o3d.utility.Vector3dVector(points_with_i[:, :3])
-    # o3d.visualization.draw_geometries([pcd])
-    np.savetxt(f'{output_directory}/{file_name}', points_with_i, delimiter=',')
+    if visualize:
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(points_with_i[:, :3])
+        o3d.visualization.draw_geometries([pcd])
+    np.savetxt(f'{OUTPUT_DIRECTORY}/{file_name}', points_with_i, delimiter=',')
 
 
 if __name__ == '__main__':
-    # change for yourself
-    source_directory = './data/source'
-    output_directory = './data/output'
-
-    for filename in os.listdir(source_directory):
+    for filename in os.listdir(SOURCE_DIRECTORY):
         if filename.endswith(".csv"):
-            rotate_csv(file_name=filename, source_directory=source_directory,
-                       output_directory=output_directory)
+            rotate_csv(file_name=filename, visualize=False)
         else:
             continue
