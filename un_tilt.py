5e283b53db2861485d313aa977be9d00a23c8e3f
Mostafa <s.mostafa.a96@gmail.com>
2021-10-21 23:34:39 -0400

    iterate over all files in a directory

--- un_tilt.py
+++ un_tilt.py
@@ -1,4 +1,7 @@
+import os
+
 import numpy as np
+
 # uncomment all the comment to visualize the point cloud
 # import open3d as o3d
 
@@ -7,8 +10,9 @@ ROTATION_MATRIX = np.array([[0.99648296, 0.03442719, -0.07639682],
                             [0.08272604, -0.25897457, 0.96233496]])
 
 
-def rotate_csv(path_to_point_cloud, out_put_directory):
+def rotate_csv(file_name, source_directory, output_directory):
     # pcd = o3d.geometry.PointCloud()
+    path_to_point_cloud = os.path.join(source_directory, file_name)
     points_with_i = np.genfromtxt(path_to_point_cloud, dtype=None, delimiter=',', skip_header=0)
     points = points_with_i[:, :3]
     rotated_points_transpose = ROTATION_MATRIX @ points.T
@@ -17,10 +21,17 @@ def rotate_csv(path_to_point_cloud, out_put_directory):
     points_with_i[:, :3] = rotated_points
     # pcd.points = o3d.utility.Vector3dVector(points_with_i[:, :3])
     # o3d.visualization.draw_geometries([pcd])
-    file_name = path_to_point_cloud.split('/')[-1]
-    np.savetxt(f'{out_put_directory}/{file_name}', points_with_i, delimiter=',')
+    np.savetxt(f'{output_directory}/{file_name}', points_with_i, delimiter=',')
 
 
 if __name__ == '__main__':
+    # change for yourself
+    source_directory = './data/source'
+    output_directory = './data/output'
 
-    rotate_csv('./data/source/xyzi_m1255_1187.csv', './data/output')
+    for filename in os.listdir(source_directory):
+        if filename.endswith(".csv"):
+            rotate_csv(file_name=filename, source_directory=source_directory,
+                       output_directory=output_directory)
+        else:
+            continue
