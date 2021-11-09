00bcde1982d3387acca95f122aa83bbd7b1e06af
Mostafa <s.mostafa.a96@gmail.com>
2021-11-09 17:26:44 -0500

    mayavi did not install! so I refactored jacob files

--- jacob_files/untilt_labels.py
+++ jacob_files/untilt_labels.py
@@ -10,43 +10,46 @@ import numpy as np
 from .utils import untilt_ptc, corners_to_center
 from . import visualize_utils as V
 
-#place your point cloud file path here
+# place your point cloud file path here
 csv_file = '/home/jacob/metrolinx_dataset/1412_1419/ouster/xyzi_m1412_1279.csv'
 
-#place your label file here
+# place your label file here
 label_file = '/home/jacob/metrolinx_dataset/1412_1419/tilted_label_1412_1419/1187-1300/label_m1412_1279.txt'
 
-#loads point cloud
+# loads point cloud
 pointclouds = np.genfromtxt(csv_file, delimiter=',')
 
-#loads rotational matrix
+# loads rotational matrix
 rot = np.genfromtxt('/home/jacob/metrolinx_2d_bb_distance/mat_rot.csv', delimiter=',')
 
 new_label_file = '/home/jacob/metrolinx_dataset/1412_1419/test/label_m1412_1279.txt'
 
 new_csv_file = '/home/jacob/metrolinx_dataset/1412_1419/untilted_outser/xyzi_m1412_1279.csv'
-#loads labels
+# loads labels
 boxes, labels, truncation = V.read_metro_linx_label_untilt(label_file)
 corners3d = V.boxes_to_corners_3d(boxes)
 
-#xyz = boxes[:,:3]
+# xyz = boxes[:,:3]
 
-#new_xyz = untilt_ptc(xyz, rot)
+# new_xyz = untilt_ptc(xyz, rot)
 new_corners = []
 for i in range(boxes.shape[0]):
     new_corners.append(untilt_ptc(corners3d[i], rot))
 new_pointclouds = untilt_ptc(pointclouds, rot)
-#converted kitti object into 8 point corners
+# converted kitti object into 8 point corners
 
 np.savetxt(new_csv_file, new_pointclouds, delimiter=",")
 label_list = []
 for i in range(len(new_corners)):
     new_box = corners_to_center(new_corners[i])
-    alpha = np.arctan2(new_box[0,1], new_box[0,0])
-    kitti_list = [str(labels[i]), str(truncation[i]), str(0), str(round(alpha,2)), 'Nan', 'Nan', 'Nan', 'Nan', str(round(new_box[0,5],2)), str(round(new_box[0,4],2)), str(round(new_box[0,3],2)), str(round(new_box[0,0],2)), str(round(new_box[0,1],2)), str(round(new_box[0,2],2)), str(round(new_box[0,6],2)-1.57)]
+    alpha = np.arctan2(new_box[0, 1], new_box[0, 0])
+    kitti_list = [str(labels[i]), str(truncation[i]), str(0), str(round(alpha, 2)), 'Nan', 'Nan',
+                  'Nan', 'Nan', str(round(new_box[0, 5], 2)), str(round(new_box[0, 4], 2)),
+                  str(round(new_box[0, 3], 2)), str(round(new_box[0, 0], 2)),
+                  str(round(new_box[0, 1], 2)), str(round(new_box[0, 2], 2)),
+                  str(round(new_box[0, 6], 2) - 1.57)]
     label_list.append(kitti_list)
-    
 
 with open(new_label_file, 'a') as out:
     for kitti_label in label_list:
-        out.write(' '.join(str(i) for i in  kitti_label) + '\n')
+        out.write(' '.join(str(i) for i in kitti_label) + '\n')
--- jacob_files/visualize_metrolinx.py
+++ jacob_files/visualize_metrolinx.py
@@ -1,38 +1,36 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 27 17:17:20 2021
-
-@author: jacob
-visualization for metrolinx dataset
-"""
-
-import numpy as np
-import mayavi.mlab as mlab
-
-from . import visualize_utils as V
-
-
-#file = 
-
-#for file in files:
-    #place your point cloud file path here
-csv_file = '/home/jacob/metrolinx_dataset/1412_1419/untilted_outser/xyzi_m1412_1279.csv'
-
-#place your label file here
-label_file = '/home/jacob/metrolinx_dataset/1412_1419/test/label_m1412_1279.txt'
-
-#loads point cloud
-pointclouds = np.genfromtxt(csv_file, delimiter=',')
-
-#loads labels
-boxes, _, _ = V.read_metro_linx_label(label_file)
-
-
-#converted kitti object into 8 point corners
-corners3d = V.boxes_to_corners_3d(boxes)
-
-#draw_scene
-V.draw_metrolinx_scene(pointclouds, corners3d)
-#mlab.savefig(filename='test.png')
-mlab.show(stop=True)
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+# """
+# Created on Wed Oct 27 17:17:20 2021
+#
+# @author: jacob
+# visualization for metrolinx dataset
+# """
+#
+# import numpy as np
+# import mayavi.mlab as mlab
+#
+# from . import visualize_utils as V
+#
+# # file =
+#
+# # for file in files:
+# # place your point cloud file path here
+# csv_file = '/home/jacob/metrolinx_dataset/1412_1419/untilted_outser/xyzi_m1412_1279.csv'
+#
+# # place your label file here
+# label_file = '/home/jacob/metrolinx_dataset/1412_1419/test/label_m1412_1279.txt'
+#
+# # loads point cloud
+# pointclouds = np.genfromtxt(csv_file, delimiter=',')
+#
+# # loads labels
+# boxes, _, _ = V.read_metro_linx_label(label_file)
+#
+# # converted kitti object into 8 point corners
+# corners3d = V.boxes_to_corners_3d(boxes)
+#
+# # draw_scene
+# V.draw_metrolinx_scene(pointclouds, corners3d)
+# # mlab.savefig(filename='test.png')
+# mlab.show(stop=True)
