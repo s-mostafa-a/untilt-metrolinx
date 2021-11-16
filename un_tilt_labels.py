194fcdb3fadbbbab5fb3e3827435be290f492600
Mostafa <s.mostafa.a96@gmail.com>
2021-11-16 16:53:49 -0500

    something that I can refer afterwards

--- un_tilt_labels.py
+++ un_tilt_labels.py
@@ -38,7 +38,7 @@ def rotate_txt(file_name):
     # print(np.mean(new_corners_xyz, axis=0))
     print(new_corners)
     print(np.mean(new_corners, axis=0))
-    new_box = JU.corners_to_center(new_corners)
+    new_box = JU.corners_to_center(new_corners).squeeze(0)
     print(new_box)
     alpha = np.arctan2(new_box[1], new_box[0])
     kitti_list = [str(labels[0]),
@@ -71,6 +71,13 @@ def rotate_txt(file_name):
 
                   # yr
                   str(round(new_box[6] - 1.57, 2))]
+    # str(round(new_box[0, 5], 2)),
+    # str(round(new_box[0, 4], 2)),
+    # str(round(new_box[0, 3], 2)),
+    # str(round(new_box[0, 0], 2)),
+    # str(round(new_box[0, 1], 2)),
+    # str(round(new_box[0, 2], 2)),
+    # str(round(new_box[0, 6], 2) - 1.57)
     with open(path_to_result_labels, 'w') as out:
         out.write(' '.join(str(i) for i in kitti_list) + '\n')
 
